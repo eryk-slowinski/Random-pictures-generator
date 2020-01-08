c836f1085532f2dc045821b516106bba2c7f1755
Eryk Slowinski <e.slowinski92@gmail.com>
2020-01-08 09:01:45 +0100

    Recent changes

--- main.js
+++ main.js
@@ -1,15 +1,18 @@
 const image = document.querySelector('.image');
 const button = document.querySelector('.generate-pic');
+const bgcDiv = document.querySelector('.bgc')
 
 const drawImage = () => {
     if (button.style.opacity !== '0') {
         const draw = Math.floor(Math.random() * 10000);
         image.src = `https://picsum.photos/800?random=${draw}`;
+        image.style.border = '2px solid black';
     }
 }
 
 const growPic = () => {
     image.classList.add('bigger-image');
+    bgcDiv.style.filter = 'blur(6px)';
     button.style.opacity = '0';
 }
 
@@ -17,6 +20,7 @@ const resizePic = () => {
     const clickOutside = !event.target.closest('img');
     if (clickOutside) {
         image.classList.remove('bigger-image');
+        bgcDiv.style.filter = 'blur(1px)';
         button.style.opacity = '1';
     }
 }
